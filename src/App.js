2aa6f7c769a6c6de47df9ad39429650cea7a32a6
Qori Imaduddin <qoriimaduddin59@gmail.com>
2025-03-21 21:31:30 +0700

    Add Footer component and update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
@@ -8,13 +9,14 @@ function App() {
       <Navbar />
       <div className="container">
         <header className="App-header">
-          <h1>Welcome to BookHaven</h1>
-          <p>Discover your literary adventure</p>
+          <h1>Welcome to Bookstore</h1>
+          <p>Find your next favorite book</p>
         </header>
         <main>
-          <p>Browse our extensive collection of books from various genres and authors.</p>
+          <p>Browse our collection of books and discover new authors.</p>
         </main>
       </div>
+      <Footer />
     </div>
   );
 }
